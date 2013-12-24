a4eb1cb6065ca8d9b52bb13b9f1a637fcdd5503d
lusia <elaleska@gmail.com>
2013-12-24 22:28:45 +0100

    Move template of one deadline action into proper templates group

--- app/handlebars/templates.js
+++ app/handlebars/templates.js
@@ -17,8 +17,6 @@ compileTemplates = function (app) {
 
     templates.about = handlebars.compile(fs.readFileSync(fFindPath('/app/views/about.hbs'), 'utf8'));
 
-    templates.one_deadline = handlebars.compile(fs.readFileSync(fFindPath('/app/views/one_deadline.hbs'), 'utf8'));
-
     templates.user = {
         login: handlebars.compile(fs.readFileSync(fFindPath('/app/views/login.hbs'), 'utf8')),
         sign: handlebars.compile(fs.readFileSync(fFindPath('/app/views/sign.hbs'), 'utf8'))
@@ -27,6 +25,7 @@ compileTemplates = function (app) {
     templates.deadline = {
         'new': handlebars.compile(fs.readFileSync(fFindPath('/app/views/add_form.hbs'), 'utf8')),
         list_belongs_to_user: handlebars.compile(fs.readFileSync(fFindPath('/app/views/deadlines_list.hbs'), 'utf8')),
+        one : handlebars.compile(fs.readFileSync(fFindPath('/app/views/one_deadline.hbs'), 'utf8')),
         vote: handlebars.compile(fs.readFileSync(fFindPath('/app/views/vote.hbs'), 'utf8'))
     };
 
